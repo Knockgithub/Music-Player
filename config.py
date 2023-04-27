e442f109cd27c831e223e7ab0cd43ddcdb302863
Knockgithub <131382142+Knockgithub@users.noreply.github.com>
2023-04-27 20:11:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-BOT_USERNAME = getenv("BOT_USERNAME", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "6024489085:AAEajd4l5OTAr6LbscEWvpnS1Z6-oNAzhfo")
+BOT_USERNAME = getenv("BOT_USERNAME", "@musicahere_bot")
 admins = {}
 API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH", "")
+API_HASH = getenv("API_HASH", "06766128d68d9c1a0d683ecfbb91de2f")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
